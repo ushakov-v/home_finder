e98780444d207df608c854289f1192832277208d
ushakov-v <vitaliy-ushak0v@ya.ru>
2023-11-18 03:05:16 +0300

    Added a "log out" button and added a design for "Log in"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
